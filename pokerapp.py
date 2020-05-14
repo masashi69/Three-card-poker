7da6091678d1a5acbb006b4f52e41e62da218f8d
Masashi Suzuki <smasa619@gmail.com>
2020-05-14 15:53:46 +0900

    Modify pokerapp.py file name

--- pokerapp.py
+++ pokerapp.py
@@ -67,7 +67,7 @@ def Judge(hands, dealer=False):
 	# Evaluation of hand
 	if len(set(suit)) == 1 and rank[1] == rank[0] + 1 and rank[2] == rank[1] + 1:
 		return {'Straight Flash': 6}, max(rank)
-	if len(set(suit)) == 1 and rank[0] == 14 and rank[1] == 2 and rank[2] == 1:
+	elif len(set(suit)) == 1 and rank[0] == 14 and rank[1] == 2 and rank[2] == 1:
 		return {'Straight Flash': 6}, max(rank)
 	elif len(set(rank)) == 1:
 		return {'Three of a kind!': 5}, max(rank)

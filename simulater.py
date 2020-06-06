c14acc737781988ad89a1f21e44c5aa57ba8ba0b
Masashi Suzuki <smasa619@gmail.com>
2020-06-06 18:22:06 +0900

    Added the hand ratio when the player wins

--- simulater.py
+++ simulater.py
@@ -3,8 +3,9 @@ import collections
 
 match_list = []
 hand_list = []
+win_list = []
 
-trial = 500000
+trial = 1000000
 
 def Trials():
 	for _ in range(trial):
@@ -17,26 +18,30 @@ def Trials():
 
 	    match_result = pokerapp.Match(player, dealer)
 	    hand_result = pokerapp.Handcheck(player).get_role()[0][0]
+
+	    if match_result == 0:
+		    win_list.append(hand_result)
 	    
 	    match_list.append(match_result)
 	    hand_list.append(hand_result)
 
 def Wins():
-	for w in collections.Counter(match_list).items():
-		if w[0] == 0:
-			print('Player {} wins.'.format(w[1]))
-		else:
-			print('Dealer {} wins.'.format(w[1]))
+	w = collections.Counter(match_list)
+	print('Player {} wins.'.format(w[0]))
+	print('Dealer {} wins.'.format(w[1]))
+	print('Draw {} times.'.format(w[2]))
 
-def Hands():
+	return w[0]
+
+def Hands(lists, times):
 
 	# Sort percntages in descending order
-	hand_dict = collections.Counter(hand_list)
+	hand_dict = collections.Counter(lists)
 	sorted_hand_dict = sorted(hand_dict.items(), key=lambda x:x[1], reverse=True)
 
 	for hand, prob in sorted_hand_dict:
 		# Convert to str to use rjust
-		p  = str(round((prob / trial) * 100, 3))
+		p  = str(round((prob / times) * 100, 3))
 
 		shaped_hand = hand.ljust(16)
 		shaped_propotion = (p + '%').rjust(10)
@@ -46,17 +51,22 @@ def Hands():
 def main():
 
 	print('Three card poker simulator start.')
-	print('The simulator trials {} times.'.format(trial))
+	print('The simulator trials {:,} times.'.format(trial))
 
 	Trials()
 
 	print('\n=== Probabliry of winning or losing ===\n')
 
-	Wins()
+	W = Wins()
 
-	print('\n=== Percentage of Players hands ===\n')
+	print('\n=== Percentage of Players all hands ===\n')
 	
-	Hands()
+	Hands(hand_list, trial)
+
+	print('\n=== Percentage of Players win({:,} times) hands ===\n'.format(W))
+	
+	Hands(win_list, W)
+
 
 if __name__ == '__main__':
 	main()

9bd353f139d7e80e162f99d777a3c2351836f822
Masashi Suzuki <smasa619@gmail.com>
2021-01-26 22:51:21 +0900

    Trial fot blit function

--- game.py
+++ game.py
@@ -8,9 +8,22 @@ pygame.display.set_caption('Three card poker')
 
 GREEN = (0, 127, 0)
 
+width, height = 100, 144
+ace1 = pygame.image.load('src/ace_of_spades.png')
+ace2 = pygame.image.load('src/ace_of_hearts.png')
+ace3 = pygame.image.load('src/ace_of_diamonds.png')
+card1 = pygame.transform.scale(ace1, (width, height))
+card2 = pygame.transform.scale(ace2, (width, height))
+card3 = pygame.transform.scale(ace3, (width, height))
+
 while True:
 
 	DISPLAYSURF.fill(GREEN)
+
+	x, y = 10, 10
+	DISPLAYSURF.blit(card1, (x,y))
+	DISPLAYSURF.blit(card2, (x + width,y))
+	DISPLAYSURF.blit(card3, (x + width * 2,y))
 
 	for event in pygame.event.get():
 		if event.type == QUIT:

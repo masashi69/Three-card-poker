adb7fc5a2676c3789fb2534fcc859d1d8ea07cbd
Masashi Suzuki <smasa619@gmail.com>
2021-01-29 20:45:47 +0900

    Added processing to draw a hand and output an image

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 from pygame.locals import *
+import pokerapp
 
 pygame.init()
 DISPLAYSURF = pygame.display.set_mode((600, 400))
@@ -8,10 +9,44 @@ pygame.display.set_caption('Three card poker')
 
 GREEN = (0, 127, 0)
 
+card_images = {'♠2': 'src/2_of_spades.png', '♠3': 'src/3_of_spades.png',
+			   '♠4': 'src/4_of_spades.png', '♠5': 'src/5_of_spades.png',
+			   '♠6': 'src/6_of_spades.png', '♠7': 'src/7_of_spades.png',
+			   '♠8': 'src/8_of_spades.png', '♠9': 'src/9_of_spades.png',
+			   '♠10': 'src/10_of_spades.png', '♠11': 'src/jack_of_spades.png',
+			   '♠12': 'src/queen_of_spades.png', '♠13': 'src/king_of_spades.png',
+			   '♠14': 'src/ace_of_spades.png', '♡2': 'src/2_of_hearts.png',
+			   '♡3': 'src/3_of_hearts.png', '♡4': 'src/4_of_hearts.png',
+			   '♡5': 'src/5_of_hearts.png', '♡6': 'src/6_of_hearts.png',
+			   '♡7': 'src/7_of_hearts.png', '♡8': 'src/8_of_hearts.png',
+			   '♡9': 'src/9_of_hearts.png', '♡10': 'src/10_of_hearts.png',
+			   '♡11': 'src/jack_of_hearts.png', '♡12': 'src/queen_of_hearts.png',
+			   '♡13': 'src/king_of_hearts.png', '♡14': 'src/ace_of_hearts.png',
+			   '♢2': 'src/2_of_diamonds.png', '♢3': 'src/3_of_diamonds.png',
+			   '♢4': 'src/4_of_diamonds.png', '♢5': 'src/5_of_diamonds.png',
+			   '♢6': 'src/6_of_diamonds.png', '♢7': 'src/7_of_diamonds.png',
+			   '♢8': 'src/8_of_diamonds.png', '♢9': 'src/9_of_diamonds.png',
+			   '♢10': 'src/10_of_diamonds.png', '♢11': 'src/jack_of_diamonds.png',
+			   '♢12': 'src/queen_of_diamonds.png', '♢13': 'src/king_of_diamonds.png',
+			   '♢14': 'src/ace_of_diamonds.png', '♣2': 'src/2_of_clubs.png',
+			   '♣3': 'src/3_of_clubs.png', '♣4': 'src/4_of_clubs.png',
+			   '♣5': 'src/5_of_clubs.png', '♣6': 'src/6_of_clubs.png',
+			   '♣7': 'src/7_of_clubs.png', '♣8': 'src/8_of_clubs.png',
+			   '♣9': 'src/9_of_clubs.png', '♣10': 'src/10_of_clubs.png',
+			   '♣11': 'src/jack_of_clubs.png', '♣12': 'src/queen_of_clubs.png',
+			   '♣13': 'src/king_of_clubs.png', '♣14': 'src/ace_of_clubs.png'}
+
 width, height = 100, 144
-ace1 = pygame.image.load('src/ace_of_spades.png')
-ace2 = pygame.image.load('src/ace_of_hearts.png')
-ace3 = pygame.image.load('src/ace_of_diamonds.png')
+
+# poker play
+deck = pokerapp.Deck()
+deck.shuffle()
+
+player_hand = pokerapp.Handout(deck)
+
+ace1 = pygame.image.load(card_images[player_hand[0]])
+ace2 = pygame.image.load(card_images[player_hand[1]])
+ace3 = pygame.image.load(card_images[player_hand[2]])
 card1 = pygame.transform.scale(ace1, (width, height))
 card2 = pygame.transform.scale(ace2, (width, height))
 card3 = pygame.transform.scale(ace3, (width, height))

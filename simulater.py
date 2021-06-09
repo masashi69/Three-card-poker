a682c1e4c1ff84ccc374dd77b341322ef34d2f86
Masashi Suzuki <smasa619@gmail.com>
2021-06-09 23:22:02 +0900

    Added checking and processing to fold

--- simulater.py
+++ simulater.py
@@ -4,6 +4,7 @@ import collections
 match_list = []
 hand_list = []
 win_list = []
+fold_list = []
 
 trial = 1000
 
@@ -26,28 +27,34 @@ def Trials(chips):
 			player_hand = pokerapp.Handcheck(player).result()
 			dealer_hand = pokerapp.Handcheck(dealer, flag=True).result()
 
-			match_result = pokerapp.Match(player_hand, dealer_hand)
+			if Foldcheck(player):
+				chips = chips - (ante + pp)
+				fold_list.append('fold')
 
-			# bonus
-			ante_b = pokerapp.Payoff(ante, player_hand[0]).ante_bonus()
-			pp_b = pokerapp.Payoff(pp, player_hand[0]).pairplus_bonus()
+			else:
+				match_result = pokerapp.Match(player_hand, dealer_hand)
 
-			if match_result == 0:
-				win_list.append(hand_result)
-			elif match_result == 2:
-				ante_b = 0
-				pp_b = 0
-		
-			match_list.append(match_result)
-			hand_list.append(hand_result)
+				# bonus
+				ante_b = pokerapp.Payoff(ante, player_hand[0]).ante_bonus()
+				pp_b = pokerapp.Payoff(pp, player_hand[0]).pairplus_bonus()
 
-			# Reduce bet chips
-			chips = chips - (ante + bet_chip + pp)
+				if match_result == 0:
+					win_list.append(hand_result)
+				elif match_result == 2:
+					ante_b = 0
+					pp_b = 0
 
-			# pay off
-			pays = pokerapp.Liquidation(match_result, dealer_hand, ante, bet_chip)
+				match_list.append(match_result)
+				hand_list.append(hand_result)
 
-			chips = chips + pays + ante_b + pp_b
+				# Reduce bet chips
+
+				chips = chips - (ante + bet_chip + pp)
+
+				# pay off
+				pays = pokerapp.Liquidation(match_result, dealer_hand, ante, bet_chip)
+
+				chips = chips + pays + ante_b + pp_b
 
 			# For debug
 			#print(hand_result, match_result, chips, pays, ante_b, pp_b)
@@ -59,9 +66,11 @@ def Trials(chips):
 
 def Wins(games):
 	w = collections.Counter(match_list)
+	f = collections.Counter(fold_list)
 	print('Player {} wins. ({:.1%})'.format(w[0], w[0] / games))
 	print('Dealer {} wins. ({:.1%})'.format(w[1], w[1] / games))
 	print('Draw {} times. ({:.1%})'.format(w[2], w[2] / games))
+	print('Fold {} times. ({:.1%})'.format(f['fold'], f['fold'] / games))
 
 	return w[0]
 
@@ -73,7 +82,7 @@ def Hands(lists, times):
 
 	for hand, prob in sorted_hand_dict:
 		# Convert to str to use rjust
-		p  = str(round((prob / times) * 100, 3))
+		p = str(round((prob / times) * 100, 3))
 
 		shaped_hand = hand.ljust(16)
 		shaped_propotion = (p + '%').rjust(10)
@@ -85,6 +94,18 @@ def Save_details(text):
 	with open('details.txt', 'w') as f:
 		f.write(text)
 
+def Foldcheck(hand):
+	if pokerapp.Handcheck(hand).result()[0][0] == 1:
+		fhand = pokerapp.Handcheck(hand).result()[1]
+
+		if fhand[2] <= 12 and fhand[1] <= 6 and fhand[0] <= 4:
+			return True
+
+		else:
+			return False
+	else:
+		return False
+
 def main():
 
 	chip = 1000
@@ -93,6 +114,7 @@ def main():
 	print('Three card poker simulator start.')
 	print('The simulator trials {:,} times.'.format(trial))
 	print('You\'ll bet ante ${} and pair plus ${} all the time.'.format(ante, pp))
+	print('You fold the game if you hand less than Q,6,4 High card.')
 
 	T, G = Trials(chip)
 
